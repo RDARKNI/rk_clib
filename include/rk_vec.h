129e72ebf707d1685e7bdbd0d1122065f19c8dba
RDARKNI <Robert.Knigge1@gmail.com>
2026-09-24 19:01:43 +0200

    cleanup msvc

--- include/rk_vec.h
+++ include/rk_vec.h
@@ -361,6 +361,7 @@ static_fun void vec_clear(Vec(void) self) {
 /// ```
 #define vec_erase_if(vec, it, pred)                                                                \
   do {                                                                                             \
+    RK__IGNWARN_MSC_BEG(C4114)                                                                     \
     typeof(vec) RK___VEC = (vec);                                                                  \
     if (!vec_count(RK___VEC)) { break; }                                                           \
     typeof(*RK___VEC)*RK___BEG = RK___VEC, *const RK___END = RK___BEG + vec_COUNT(RK___BEG);       \
@@ -369,6 +370,7 @@ static_fun void vec_clear(Vec(void) self) {
       if (!(pred)) { *RK___BEG++ = *RK___IT; }                                                     \
     }                                                                                              \
     vec_COUNT(RK___VEC) = (size_t)(RK___BEG - RK___VEC);                                           \
+    RK__IGNWARN_MSC_END()                                                                          \
   } while (0)
 
 /// @brief Reverse the elements of a Vec in place.
